39aebe190c15e81b4518a040818487c7a75683f1
Yurii Osiptsov <yosiptsov@gmail.com>
2026-08-25 14:14:52 +0300

    refactor: enhance registration negative tests with test steps and add duplicate email conflict scenario

--- tests/specs/auth/userRegister.spec.ts
+++ tests/specs/auth/userRegister.spec.ts
@@ -71,8 +71,19 @@ test.describe("Negative Validation Scenarios - 400 Bad Request", () => {
   test.describe("UserReg 02: User payload missing required fields", { tag: [TAG.negative] }, () => {
     for (const { description, payload } of userWithMissedFields) {
       test(`Register fails when ${description}`, async ({ apiController }) => {
-        const response = await apiController.oAuthController.registerUser(payload as any);
-        expect(response.status()).toBe(400);
+        //Act
+        const response = await test.step("trying to register a user", async () => {
+          return await apiController.oAuthController.registerUser(payload as any);
+        });
+        //Assert
+        await test.step("response status is 400 (Bad Request)", () => {
+          expect(response.status(), "Check status").toBe(400);
+          expect(response.statusText(), "Check status message").toBe("Bad Request");
+        });
+
+        await test.step("verify response Headers", async () => {
+          verifyHeaders(response);
+        });
       });
     }
   });
@@ -80,31 +91,99 @@ test.describe("Negative Validation Scenarios - 400 Bad Request", () => {
   test.describe("UserReg 03: Invalid name constraints  ", { tag: [TAG.negative] }, () => {
     for (const { description, payload } of invalidUserName) {
       test(`Register fails when ${description}`, async ({ apiController }) => {
-        const response = await apiController.oAuthController.registerUser(payload as any);
-        expect(response.status()).toBe(400);
+        //Act
+        const response = await test.step("trying to register a user", async () => {
+          return await apiController.oAuthController.registerUser(payload as any);
+        });
+        //Assert
+        await test.step("response status is 400 (Bad Request)", () => {
+          expect(response.status(), "Check status").toBe(400);
+          expect(response.statusText(), "Check status message").toBe("Bad Request");
+        });
+
+        await test.step("verify response Headers", async () => {
+          verifyHeaders(response);
+        });
       });
     }
   });
   test.describe("UserReg 04: Invalid email format", { tag: [TAG.negative] }, () => {
     for (const { description, payload } of invalidUserEmail) {
       test(`Register fails when ${description}`, async ({ apiController }) => {
-        const response = await apiController.oAuthController.registerUser(payload as any);
-        expect(response.status()).toBe(400);
+        //Act
+        const response = await test.step("trying to register a user", async () => {
+          return await apiController.oAuthController.registerUser(payload as any);
+        });
+        //Assert
+        await test.step("response status is 400 (Bad Request)", () => {
+          expect(response.status(), "Check status").toBe(400);
+          expect(response.statusText(), "Check status message").toBe("Bad Request");
+        });
+
+        await test.step("verify response Headers", async () => {
+          verifyHeaders(response);
+        });
       });
     }
   });
   test.describe("UserReg 05: Invalid password complexity", { tag: [TAG.negative, TAG.regression] }, () => {
     for (const { description, payload } of invalidPasswordComplexity) {
       test(`Register fails when ${description}`, async ({ apiController }) => {
-        const response = await apiController.oAuthController.registerUser(payload as any);
-        expect(response.status()).toBe(400);
+        //Act
+        const response = await test.step("trying to register a user", async () => {
+          return await apiController.oAuthController.registerUser(payload as any);
+        });
+        //Assert
+        await test.step("response status is 400 (Bad Request)", () => {
+          expect(response.status(), "Check status").toBe(400);
+          expect(response.statusText(), "Check status message").toBe("Bad Request");
+        });
+
+        await test.step("verify response Headers", async () => {
+          verifyHeaders(response);
+        });
       });
     }
   });
 });
 
-test.describe("Business Logic / Conflict Scenarios - 409 Conflicts", () => {
+test.describe("Business Logic / Conflict Scenarios - 409 Conflicts", { tag: [TAG.negative, TAG.regression] }, () => {
   test.use({
     options: { isAuthorized: false, scope: [] },
+  });
+
+  const newUserPayload: RegisterUserPayload = {
+    user: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 10 }) + "A1",
+    },
+  };
+  let firstUserResponse: APIResponse;
+
+  test.beforeEach(async ({ apiController }) => {
+    //Arrange
+    firstUserResponse = await apiController.oAuthController.registerUser(newUserPayload);
+    verifyUserExistsInDB(newUserPayload.user.name, newUserPayload.user.email);
+  });
+
+  test.afterEach(async () => {
+    await deleteCreatedUserFromDB(newUserPayload.user.email);
+  });
+
+  test("UserReg 06: Duplicate email registration", async ({ apiController }) => {
+    //Act
+    const secondUserResponse = await test.step("trying to register a user already existing in DB", async () => {
+      return await apiController.oAuthController.registerUser(newUserPayload);
+    });
+    // Assert
+    await test.step("response status is 409 (Conflict)", () => {
+      expect(secondUserResponse.status(), "Check status").toBe(409);
+      expect(secondUserResponse.statusText(), "Check status message").toBe("Conflict");
+    });
+
+    await test.step("verify response Headers", async () => {
+      verifyHeaders(secondUserResponse);
+    });
   });
 });
